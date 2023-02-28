0f9cbc0ede1bbbd5f4fb886df5a88b6597f36a05
Subhan Khan <subhank1997@gmail.com>
2023-02-28 11:01:48 -0500

    worked on global styling as well as css for some homepage components

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,16 @@ import React from 'react'
 
 const index = () => {
   return (
-    <div>index</div>
+    <div>
+      <div className='products-heading'>
+        <h2>Best Selling Books</h2>
+        <p>Books of different genres</p>
+      </div>
+
+      <div className='products-container'>
+        {['Insert Product','Insert Product'].map((product) => product)}
+      </div>
+    </div>
   )
 }
 
