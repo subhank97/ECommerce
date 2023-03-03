0ccd9d420ca6b85a4d55325bdf4f99af5787999c
Subhan Khan <subhank1997@gmail.com>
2023-03-02 22:49:28 -0500

    generated product images, title, and price to show on homepage

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import { urlFor } from '@/lib/client';
 
-const Product = () => {
+const Product = ({ product: {image, title, author, slug, price, details} }) => {
   return (
-    <div>Product</div>
+    <div>
+        <Link href={`/product/${slug.current}`}>
+            <div className='product-card'>
+                <img className='product-image' src={urlFor(image && image[0])} alt='book-cover' width={300} height={250}/>
+                <p className='product-title'>{title}</p>
+                <p className='product-price'>{price}</p>
+            </div>
+        </Link>
+    </div>
   )
 }
 
