9421dd70e4a1d9f4ef6a6be2a7209fd73d702553
Subhan Khan <subhank1997@gmail.com>
2023-03-03 17:37:33 -0500

    added layout component to _app.js

---
+++ components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Head from 'next/head';
+
+
+const Layout = () => {
+  return (
+    <div className='layout'>
+        <Head>
+            <title>Subi's BookStore</title>
+        </Head>
+    </div>
+  )
+}
+
+export default Layout
--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
 export { default as Banner } from './Banner' 
 export { default as Product } from './Product' 
 export { default as Footer } from './Footer' 
+export { default as Layout } from './Layout' 
