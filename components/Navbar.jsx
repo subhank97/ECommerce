2012c97041856b2172ab37b9ecc7c125f7a3552b
Subhan Khan <subhank1997@gmail.com>
2023-03-03 20:03:32 -0500

    created layout for navabar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import Link from 'next/link'
+import { AiOutlineShopping } from 'react-icons/ai'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='navbar-container'>
+      <p className='logo'>
+        <Link href='/'>Subi's Sneaker Shop</Link>
+      </p>
+      <button onClick='' type='button' className='cart-icon'>
+        <AiOutlineShopping />
+        <span className='cart-quantity'>1</span>
+      </button>
+    </div>
   )
 }
 
