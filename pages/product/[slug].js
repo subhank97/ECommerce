d53254f3e35327f952e50db87a411816400b91a9
Subhan Khan <subhank1997@gmail.com>
2023-03-03 21:27:02 -0500

    added getStaticPaths and getStaticProps to [slug].js page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -1,9 +1,56 @@
 import React from 'react'
+import { client, urlFor } from '@/lib/client'
 
-const ProductDetails = () => {
+const ProductDetails = ({ product, products }) => {
+
+    const {image, name, price, details} = product;
+
   return (
-    <div>Product</div>
+    <div>
+        <div className='product-deatils-container'>
+            <div>
+                <div className='image-container'>
+                    <img src={urlFor(image[0])} alt='shoe-style' />
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
+export const getStaticPaths = async () => {
+    const query = `*[_type == "product"] {
+      slug {
+        current
+      }
+    }
+    `;
+  
+    const products = await client.fetch(query);
+  
+    const paths = products.map((product) => ({
+      params: { 
+        slug: product.slug.current
+      }
+    }));
+  
+    return {
+      paths,
+      fallback: 'blocking'
+    }
+  }
+  
+  export const getStaticProps = async ({ params: { slug }}) => {
+    const query = `*[_type == "product" && slug.current == '${slug}'][0]`;
+    const productsQuery = '*[_type == "product"]'
+    
+    const product = await client.fetch(query);
+    const products = await client.fetch(productsQuery);
+  
+    console.log(product);
+  
+    return {
+      props: { products, product }
+    }
+  }
 
 export default ProductDetails
