765cf11288380d6d486ec616690b08edf334a603
Subhan Khan <subhank1997@gmail.com>
2023-03-06 22:10:00 -0500

    css for cart component

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import Link from 'next/link'
+import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai'
+import {TiDeleteOutline} from 'react-icons/ti'
+import { Toast } from 'react-hot-toast'
+import { useStateContext } from '@/context/StateContext'
+import { urlFor } from '@/lib/client'
 
 const Cart = () => {
+
+    const cartRef = useRef();
+    const {totalPrice, totalQuantities, cartItems, setShowCart} = useStateContext();
+
   return (
-    <div>Cart</div>
+    <div className='cart-wrapper' ref={cartRef}>
+        <div className='cart-container'>
+            <button type='button' className='cart-heading' onClick={() => setShowCart(false)}>
+                <AiOutlineLeft />
+                <span className='heading'>Your Cart</span>
+                <span className='number-of-items'>{totalQuantities}</span>
+            </button>
+        </div>
+    </div>
   )
 }
 
