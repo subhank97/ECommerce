ebe1cc745e11e84b0ec2d0c8ce1d943d6e965906
Subhan Khan <subhank1997@gmail.com>
2023-03-07 19:44:16 -0500

    added checkout feature to navigate to stripe checkout page

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -2,11 +2,30 @@ import React, { useRef } from 'react'
 import Link from 'next/link'
 import { AiOutlineMinus, AiOutlinePlus, AiOutlineLeft, AiOutlineShopping } from 'react-icons/ai'
 import { TiDeleteOutline } from 'react-icons/ti'
-import { Toast } from 'react-hot-toast'
 import { useStateContext } from '@/context/StateContext'
 import { urlFor } from '@/lib/client'
+import getStripe from '@/lib/getStripe'
+import toast from 'react-hot-toast'
 
 const Cart = () => {
+
+    const handleCheckout = async () => {
+        const stripe = await getStripe();
+        const response = await fetch('/api/stripe', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(cartItems),
+        });
+        if(response.statusCode === 500) return;
+
+        const data = await response.json();
+
+        toast.loading('Redirecting...');
+        
+        stripe.redirectToCheckout({ sessionId: data.id });
+      }
 
     const cartRef = useRef();
     const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuantity, onRemoveFromCart } = useStateContext();
@@ -62,7 +81,7 @@ const Cart = () => {
                             <h3>${totalPrice}</h3>
                         </div>
                         <div className='checkout-button-container'>
-                            <button type='button' className='checkout-button' onClick=''>
+                            <button type='button' className='checkout-button' onClick={handleCheckout}>
                                 Pay with Stripe
                             </button>
                         </div>
--- pages/api/Stripe.js
+++ pages/api/Stripe.js
@@ -1,4 +1,6 @@
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
+import Stripe from 'stripe';
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
@@ -6,26 +8,40 @@ export default async function handler(req, res) {
       const params = {
         submit_type: 'pay',
         mode: 'payment',
-        payment_method_type: ['cart'],
+        payment_method_types: ['card'],
         billing_address_collection: 'auto',
         shipping_options: [
-            {shipping_rate: 'shr_1Mj8rtBkMWIR9EnBV76aZpmH'},
-            {shipping_rate: 'shr_1Mj8unBkMWIR9EnBuPKElLu4'}
+          { shipping_rate: 'shr_1Mj8rtBkMWIR9EnBV76aZpmH' },
+          { shipping_rate: 'shr_1Mj8unBkMWIR9EnBuPKElLu4' },
         ],
-        line_items: [
-          {
-            // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-            price: '{{PRICE_ID}}',
-            quantity: 1,
-          },
-        ],
-        mode: 'payment',
-        success_url: `${req.headers.origin}/?success=true`,
-        cancel_url: `${req.headers.origin}/?canceled=true`,
+        line_items: req.body.map((item) => {
+          const img = item.image[0].asset._ref;
+          const newImage = img.replace('image-', 'https://cdn.sanity.io/images/vfxfwnaw/production/').replace('-webp', '.webp');
+
+          return {
+            price_data: { 
+              currency: 'usd',
+              product_data: { 
+                name: item.name,
+                images: [newImage],
+              },
+              unit_amount: item.price * 100,
+            },
+            adjustable_quantity: {
+              enabled:true,
+              minimum: 1,
+            },
+            quantity: item.quantity
+          }
+        }),
+        success_url: `${req.headers.origin}/success`,
+        cancel_url: `${req.headers.origin}/canceled`,
       }
+
       // Create Checkout Sessions from body params.
       const session = await stripe.checkout.sessions.create(params);
-      res.redirect(303, session.url);
+
+      res.status(200).json(session);
     } catch (err) {
       res.status(err.statusCode || 500).json(err.message);
     }
