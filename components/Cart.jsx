7c9fe2f89459fb9d7d353df9d4ef21119c10d888
Subhan Khan <subhank1997@gmail.com>
2023-03-11 17:18:16 -0500

    sanity checkout

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -18,9 +18,9 @@ const Cart = () => {
           },
           body: JSON.stringify(cartItems),
         });
-        if(response.statusCode === 500) return;
-        const data = await response.json();
-        toast.loading('Redirecting...');
+        if(response.statusCode === 500) return;        
+        const data = await response.json();   
+        toast.loading('Redirecting...');  
         stripe.redirectToCheckout({ sessionId: data.id });
       }
 
