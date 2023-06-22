6823e3510d29dacd86224bb36ed6a39f27629bf3
Subhan Khan <subhank1997@gmail.com>
2023-06-22 16:32:20 -0400

    updated styling for product page

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -16,8 +16,8 @@ const ProductDetails = ({ product, products }) => {
   }
 
   return (
-    <div>
-      <div className="md:flex gap-10 m-10 mt-12 text-blue-800 flex-col md:flex-row">
+    <div className='lg:pt-10'>
+      <div className="md:flex gap-10 m-10 mt-12 text-slate-800 flex-col md:flex-row">
         <div className='w-3/4 '>
           <div className="pb-5 max-w-xl">
             <img src={urlFor(image && image[index])} className="w-full h-80 max-w-full h-64 object-contain cursor-pointer transition-all duration-300" />
@@ -32,33 +32,33 @@ const ProductDetails = ({ product, products }) => {
         </div>
 
         <div className="w-full mt-5 md:mt-0 md:ml-12">
-          <h1 className="text-4xl">{name}</h1>
+          <h1 className="pb-3 font-bold text-5xl">{name}</h1>
           <div className="text-xl text-orange-500 mt-2.5 gap-1 items-center">
-            <div className='flex'>
+            <div className='flex pb-5'>
               <AiFillStar />
               <AiFillStar />
               <AiFillStar />
               <AiFillStar />
               <AiOutlineStar />
             </div>
-            <p>
-              (20)
-            </p>
           </div>
-          <h4 className="mt-2.5 text-sm">Details: </h4>
-          <p>{details}</p>
-          <p className="font-bold text-5xl mt-7.5 text-red-700">${price}</p>
-          <div className="text-blue-800 flex gap-5 mt-2.5 items-center w-72">
-            <h3>Quantity:</h3>
-            <p className="border border-orange-500 px-1.5">
-              <span className="text-lg px-2.5 py-1.5 cursor-pointer text-orange-300" onClick={decreaseQuantity}><AiOutlineMinus /></span>
-              <span className="border-r border-l border-orange-500 text-xl">{quantity}</span>
-              <span className="text-lg px-2.5 py-1.5 cursor-pointer text-orange-300" onClick={increaseQuantity}><AiOutlinePlus /></span>
-            </p>
-          </div>
-          <div className="flex gap-7.5">
-            <button type="button" className="py-2.5 px-5 border border-red-700 mt-10 text-lg font-medium bg-white text-red-700 cursor-pointer w-48 transform transition-transform duration-500 hover:scale-110" onClick={() => onAddToCart(product, quantity)}>Add to Cart</button>
-            <button type="button" className="w-48 py-2.5 px-5 bg-red-700 text-white border-none mt-10 text-lg font-medium cursor-pointer transform transition-transform duration-500 hover:scale-110" onClick={handleBuyNow}>Buy Now</button>
+          <p className="italic font-light mt-2.5 text-sm">{details}</p>
+          <p className="pt-10 pb-10 font-bold text-3xl mt-7.5 text-slate-700">${price}</p>
+
+          <div class="flex gap-10"> 
+            <div class="custom-number-input h-10 w-32">
+              <div class="flex flex-row h-full w-full rounded-lg relative bg-transparent mt-1">
+                <button data-action="decrement" class="border border-black  text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-l cursor-pointer outline-none">
+                  <span class="m-auto text-2xl font-thin" onClick={decreaseQuantity}> - </span>
+                </button>
+                <input class="border border-black outline-none focus:outline-none text-center w-full  font-semibold text-md md:text-basecursor-default flex items-center text-gray-700  outline-none" value={quantity} />
+                <button data-action="increment" class="border border-black  text-gray-600 hover:text-gray-700 hover:bg-gray-400 h-full w-20 rounded-r cursor-pointer">
+                  <span class="m-auto text-2xl font-thin" onClick={increaseQuantity}> + </span>
+                </button>
+              </div>
+            </div>
+
+            <button type="button" className="font-bold py-2.5 px-5 bg-custom-rose mt-1 text-lg font-medium text-black cursor-pointer w-48 transform transition-transform duration-500 hover:scale-110" onClick={() => onAddToCart(product, quantity)}>Add to Cart</button>
           </div>
         </div>
       </div>
@@ -78,11 +78,11 @@ const ProductDetails = ({ product, products }) => {
 }
 export const getStaticPaths = async () => {
   const query = `*[_type == "product"] {
-      slug {
+        slug {
         current
       }
     }
-    `;
+      `;
 
   const products = await client.fetch(query);
 
