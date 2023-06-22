13e5e4aae1641bcd17ee9b457f74d7079b2b0671
Subhan Khan <subhank1997@gmail.com>
2023-06-22 00:33:25 -0400

    updated footer

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -3,17 +3,18 @@ import React from 'react'
 
 const FooterBanner = ({ footer }) => {
   return (
-    <div className='footer-banner-container'>
-        <div className='f-description'>
-            <div className='left'>
-                <p>{footer.discount}</p>
-                <h3>{footer.midText}</h3>
-                <h3>{footer.largeText}</h3>
-                <p>{footer.saleTime}</p>
-            </div>
-            <img className='footer-image' src={urlFor(footer.image)} alt='shoe' />
-        </div>
-    </div>
+    <></>
+    // <div className='footer-banner-container'>
+    //     <div className='f-description'>
+    //         <div className='left'>
+    //             <p>{footer.discount}</p>
+    //             <h3>{footer.midText}</h3>
+    //             <h3>{footer.largeText}</h3>
+    //             <p>{footer.saleTime}</p>
+    //         </div>
+    //         <img className='footer-image' src={urlFor(footer.image)} alt='shoe' />
+    //     </div>
+    // </div>
   )
 }
 
